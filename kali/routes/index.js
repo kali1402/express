37da4a89fed46349185216849cd1c1865956f392
kali1402 <amcopeew68@naver.com>
2019-05-21 16:38:17 +0900

    서버

---
+++ kali/routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// 로그인 페이지 접근
+router.get('/login', function(req, res, next) {
+  console.log(req.session);
+    res.render('login', { title: '로그인' });
+});
+
+router.get('/god', function(req, res, next) {
+  if(req.session.ID && req.session.PW) {
+    const params = {
+      ID: req.session.ID,
+      PW: req.session.PW
+    };
+    res.render('god', params);
+  } else {
+    res.render('login', { title: '로그인' });
+  }
+})
+// 로그인 확인 페이지 접근
+router.post('/login', function(req, res, next){
+
+  const params = {
+    ID: req.body.id,
+    PW: req.body.pw
+  };
+
+  req.session.ID = params.ID;
+  req.session.PW = params.PW;
+  console.log(req.session);
+
+  res.render('login2', params);
+})
+
+//로그아웃
+router.get('/logout', function(req, res, next) {
+  res.render('logout');
+  req.session.destroy();
+})
+module.exports = router;
